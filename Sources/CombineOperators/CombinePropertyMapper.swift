1c29ecc0f6d3d183c6382b9f8f222c4933cc7d05
Danil <voidilov@gmail.com>
2021-02-28 16:35:58 +0300

    1.3.0

--- Sources/CombineOperators/CombinePropertyMapper.swift
+++ Sources/CombineOperators/CombinePropertyMapper.swift
@@ -10,7 +10,8 @@ import Combine
 
 @available(iOS 13.0, macOS 10.15, *)
 @dynamicMemberLookup
-public struct CombinePropertyMapper<Base: Publisher, Output, Failure: Error>: Publisher {
+public struct CombinePropertyMapper<Base: Publisher, Output>: Publisher {
+	public typealias Failure = Base.Failure
 	private let base: Base
 	private let keyPath: KeyPath<Base.Output, Output>
 	
@@ -19,17 +20,17 @@ public struct CombinePropertyMapper<Base: Publisher, Output, Failure: Error>: Pu
 		self.keyPath = keyPath
 	}
 	
-	public subscript<T>(dynamicMember keyPath: KeyPath<Output, T>) -> CombinePropertyMapper<Base, T, Failure> {
-		CombinePropertyMapper<Base, T, Failure>(base, for: self.keyPath.appending(path: keyPath))
+	public subscript<T>(dynamicMember keyPath: KeyPath<Output, T>) -> CombinePropertyMapper<Base, T> {
+		CombinePropertyMapper<Base, T>(base, for: self.keyPath.appending(path: keyPath))
 	}
 	
 	public func receive<S: Subscriber>(subscriber: S) where Failure == S.Failure, Output == S.Input {
-		
+		base.map(keyPath).receive(subscriber: subscriber)
 	}
 	
 }
 
 @available(iOS 13.0, macOS 10.15, *)
 extension Publisher {
-	public var mp: CombinePropertyMapper<Self, Output, Failure> { CombinePropertyMapper(self, for: \.self) }
+	public var mp: CombinePropertyMapper<Self, Output> { CombinePropertyMapper(self, for: \.self) }
 }
