5b6bd5ac3d29a8c54d2d641c30752943c5a15eaa
Danil <voidilov@gmail.com>
2021-03-04 18:39:43 +0300

    1.61.0

--- CombineOperators.podspec
+++ CombineOperators.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'CombineOperators'
-  s.version          = '1.60.0'
+  s.version          = '1.61.0'
   s.summary          = 'A short description of CombineOperators.'
   s.description      = <<-DESC
 TODO: Add long description of the pod here.
--- Sources/CombineOperators/Replay.swift
+++ Sources/CombineOperators/Replay.swift
@@ -10,42 +10,88 @@ import Combine
 
 @available(iOS 13.0, macOS 10.15, *)
 public final class ReplaySubject<Output, Failure: Error>: Subject {
-	private var recording = Record<Output, Failure>.Recording()
-	private let stream = PassthroughSubject<Output, Failure>()
-	private let maxValues: Int
+	private var buffer = [Output]()
+	private let bufferSize: Int
+	private var subscriptions = [ReplaySubjectSubscription<Output, Failure>]()
+	private var completion: Subscribers.Completion<Failure>?
+	private let lock = NSRecursiveLock()
 	
-	public init(maxValues: Int = 0) {
-		self.maxValues = maxValues
+	public init(_ bufferSize: Int = 0) {
+		self.bufferSize = bufferSize
 	}
 	
+	/// Provides this Subject an opportunity to establish demand for any new upstream subscriptions
 	public func send(subscription: Subscription) {
-		subscription.request(maxValues == 0 ? .unlimited : .max(maxValues))
+		lock.lock(); defer { lock.unlock() }
+		subscription.request(.unlimited)
 	}
 	
+	/// Sends a value to the subscriber.
 	public func send(_ value: Output) {
-		recording.receive(value)
-		stream.send(value)
-		if recording.output.count == maxValues {
-			send(completion: .finished)
-		}
+		lock.lock(); defer { lock.unlock() }
+		buffer.append(value)
+		buffer = buffer.suffix(bufferSize)
+		subscriptions.forEach { $0.receive(value) }
 	}
 	
+	/// Sends a completion signal to the subscriber.
 	public func send(completion: Subscribers.Completion<Failure>) {
-		recording.receive(completion: completion)
-		stream.send(completion: completion)
+		lock.lock(); defer { lock.unlock() }
+		self.completion = completion
+		subscriptions.forEach { subscription in subscription.receive(completion: completion) }
 	}
 	
-	public func receive<S>(subscriber: S) where S: Subscriber, Failure == S.Failure, Output == S.Input {
-		Record(recording: self.recording)
-			.append(self.stream)
-			.receive(subscriber: subscriber)
+	/// This function is called to attach the specified `Subscriber` to the`Publisher
+	public func receive<Downstream: Subscriber>(subscriber: Downstream) where Downstream.Failure == Failure, Downstream.Input == Output {
+		lock.lock(); defer { lock.unlock() }
+		let subscription = ReplaySubjectSubscription<Output, Failure>(downstream: AnySubscriber(subscriber))
+		subscriber.receive(subscription: subscription)
+		subscriptions.append(subscription)
+		subscription.replay(buffer, completion: completion)
+	}
+}
+
+public final class ReplaySubjectSubscription<Output, Failure: Error>: Subscription {
+	private let downstream: AnySubscriber<Output, Failure>
+	private var isCompleted = false
+	private var demand: Subscribers.Demand = .none
+	
+	public init(downstream: AnySubscriber<Output, Failure>) {
+		self.downstream = downstream
 	}
 	
+	// Tells a publisher that it may send more values to the subscriber.
+	public func request(_ newDemand: Subscribers.Demand) {
+		demand += newDemand
+	}
+	
+	public func cancel() {
+		isCompleted = true
+	}
+	
+	public func receive(_ value: Output) {
+		guard !isCompleted, demand > 0 else { return }
+		
+		demand += downstream.receive(value)
+		demand -= 1
+	}
+	
+	public func receive(completion: Subscribers.Completion<Failure>) {
+		guard !isCompleted else { return }
+		isCompleted = true
+		downstream.receive(completion: completion)
+	}
+	
+	public func replay(_ values: [Output], completion: Subscribers.Completion<Failure>?) {
+		guard !isCompleted else { return }
+		values.forEach { value in receive(value) }
+		if let completion = completion { receive(completion: completion) }
+	}
 }
 
 @available(iOS 13.0, macOS 10.15, *)
 extension Publisher {
-	public func share(replay maxValues: Int = 0) -> AnyPublisher<Output, Failure> {
-		multicast(subject: ReplaySubject(maxValues: maxValues)).autoconnect().eraseToAnyPublisher()
+	public func share(replay bufferSize: Int = 0) -> AnyPublisher<Output, Failure> {
+		multicast(subject: ReplaySubject(bufferSize)).autoconnect().eraseToAnyPublisher()
 	}
 }
